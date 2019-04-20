ecc580f44b1c7b0456d7a64a290cb0cd19b05ce0
Ahmed Taha <ahmedashraf@aucegypt.edu>
2019-04-20 12:23:47 +0200

    Solved the logical error and some of the syntax ones , test 1 passed now each cell of the 2d grid consists a list [0,1,1] for example which indicates the paths taken. also removed SquareGrid.py as it wasn't useful i changed it completely it was just a start for the project.

--- Test1.py
+++ Test1.py
@@ -7,6 +7,11 @@
 
 import matplotlib.pyplot as plt
 import astarRouter
+from mpl_toolkits import mplot3d
+import numpy as np
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
 
 
 test = astarRouter.astarRouter(5, 5, 2)
@@ -16,14 +21,13 @@ d,e,f=test.route(0,0,2,1,2,3)
 print(a)
 print(b)
 print(c)
-
 print(" ")
-
-
 
 print(d)
 print(e)
 print(f)
+
+print("\n")
 
 test.printgrid()
 
--- astarRouter.py
+++ astarRouter.py
@@ -6,6 +6,7 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class astarRouter:
@@ -15,10 +16,10 @@ class astarRouter:
         self.grid_w = grid_w
         self.grid_h = grid_h
         self.via_cost = via_cost
-        self.grid = np.empty([self.grid_w, self.grid_h], dtype=int)
+        self.grid = np.empty([self.grid_w, self.grid_h], dtype=object)
         for i in range(self.grid_h):
             for j in range(self.grid_w):
-                self.grid[i][j] = 0
+                self.grid[i][j] = [0, 0, 0]
 
     def heuristic_function(self,source_x, source_y, source_layer, target_x, target_y, target_layer):
 
@@ -41,13 +42,13 @@ class astarRouter:
             S = (source_x + 1, source_y , target_layer)
             E = (source_x, source_y + 1 , source_layer)
 
-            if N[0] >= 0 and self.grid[N[0]][N[1]] != target_layer+10:
+            if N[0] >= 0 and self.grid[N[0]][N[1]][1] != 1 and self.grid[N[0]][N[1]][2] != 1:
                 finalnodes.append(N)
-            if W[1] >= 0 and self.grid[W[0]][W[1]] != source_layer+10:
+            if W[1] >= 0 and self.grid[W[0]][W[1]][0] != 1:
                 finalnodes.append(W)
-            if S[0] < self.grid_h and self.grid[S[0]][S[1]] != target_layer+10:
+            if S[0] < self.grid_h and self.grid[S[0]][S[1]][1] != 1 and self.grid[S[0]][S[1]][2] != 1:
                 finalnodes.append(S)
-            if E[1] < self.grid_w and self.grid[E[0]][E[1]] != source_layer+10:
+            if E[1] < self.grid_w and self.grid[E[0]][E[1]][0] != 1:
                 finalnodes.append(E)
 
         elif source_layer == 2:
@@ -57,15 +58,14 @@ class astarRouter:
             S = (source_x + 1, source_y, source_layer)
             E = (source_x, source_y + 1, target_layer)
 
-            if N[0] >= 0 and self.grid[N[0]][N[1]] != source_layer+10:
+            if N[0] >= 0 and self.grid[N[0]][N[1]][1] != 1:
                 finalnodes.append(N)
-            if W[1] >= 0 and self.grid[W[0]][W[1]] != target_layer+10:
+            if W[1] >= 0 and self.grid[W[0]][W[1]][0] != 1 or self.grid[W[0]][W[1]][2] != 1:
                 finalnodes.append(W)
-            if S[0] < self.grid_h and self.grid[S[0]][S[1]] != source_layer+10:
+            if S[0] < self.grid_h and self.grid[S[0]][S[1]][1] != 1:
                 finalnodes.append(S)
-            if E[1] < self.grid_w and self.grid[E[0]][E[1]] != target_layer+10:
+            if E[1] < self.grid_w and self.grid[E[0]][E[1]][0] != 1 or self.grid[E[0]][E[1]][2] != 1 :
                 finalnodes.append(E)
-
         else:
 
             N = (source_x - 1, source_y, source_layer)
@@ -73,14 +73,14 @@ class astarRouter:
             S = (source_x + 1, source_y, source_layer)
             E = (source_x, source_y + 1, target_layer)
 
-            if N[0] >= 0 and self.grid[N[0]][N[1]] != target_layer+10:
-                finalnodes.append(N)
-            if W[1] >= 0 and self.grid[W[0]][W[1]] != source_layer+10:
-                finalnodes.append(W)
-            if S[0] < self.grid_h and self.grid[S[0]][S[1]] != target_layer+10:
-                finalnodes.append(S)
-            if E[1] < self.grid_w and self.grid[E[0]][E[1]] != source_layer+10:
-                finalnodes.append(E)
+        if N[0] >= 0 and self.grid[N[0]][N[1]][0] != 1 and self.grid[N[0]][N[1]][1] != 1:
+            finalnodes.append(N)
+        if W[1] >= 0 and self.grid[W[0]][W[1]][2] != 1:
+            finalnodes.append(W)
+        if S[0] < self.grid_h and self.grid[S[0]][S[1]][0] != 1 and self.grid[S[0]][S[1]][1] != 1:
+            finalnodes.append(S)
+        if E[1] < self.grid_w and self.grid[E[0]][E[1]][2] != 1:
+            finalnodes.append(E)
 
         if len(finalnodes) == 0:
             print('Cannot find a next node, so cannot find a path')
@@ -96,15 +96,14 @@ class astarRouter:
 
                 elif source_layer != i[2]:
 
-                    f = self.via_cost+ 1 + self.heuristic_function(i[0],i[1],i[2],target_x,target_y,target_layer)
+                    f = self.via_cost + 1 + self.heuristic_function(i[0],i[1],i[2],target_x,target_y,target_layer)
 
                 if f < nn:
                     nn = f
                     chosen_node = i
                     g = f - self.heuristic_function(i[0],i[1],i[2],target_x,target_y,target_layer)
 
-        return chosen_node,nn,g
-
+        return chosen_node, nn, g
 
     def route(self,source_x, source_y, source_layer, target_x, target_y, target_layer):
 
@@ -119,17 +118,18 @@ class astarRouter:
         h.append(self.heuristic_function(source_x, source_y, source_layer, target_x, target_y, target_layer))
         f.append(g[0]+h[0])
         if source_layer == 1:
-            self.grid[source_x][source_y] = 11
+            self.grid[source_x][source_y][0] = 1
         elif source_layer == 2:
-            self.grid[source_x][source_y] = 12
-        else: self.grid[source_x][source_y] = 13
+            self.grid[source_x][source_y][1] = 1
+        else:
+            self.grid[source_x][source_y][2] = 1
 
         current_node = path[0]
 
         while not arrived:
             current_node_,f_val,g_val = self.next_node(current_node[0], current_node[1], current_node[2], target_x, target_y, target_layer)
             current_node = current_node_
-            self.grid[current_node[0]][current_node[1]] = 10+current_node[2]
+            self.grid[current_node[0]][current_node[1]][current_node[2]-1] = 1
             path.append(current_node)
             f.append(f_val)
             g.append(g_val)
@@ -141,4 +141,13 @@ class astarRouter:
         return path,g,f
 
     def printgrid(self):
-        print(self.grid)
+
+        for i in range(self.grid_h):
+            for j in range(self.grid_w):
+                if j == self.grid_w-1:
+                    print(self.grid[i][j], end=" ")
+                else:
+                    print(self.grid[i][j], end=",")
+            print("\n")
+
+
